c3db92e4dae919e6a7e802ad2e71cbbc9d167529
Arjun K Varkey <arjunkvarkey@gmail.com>
2021-10-06 18:33:34 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,22 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-    response.json().then((data)=>{
-        console.log(data)
-    })
-})
+// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
+//     response.json().then((data)=>{
+//         console.log(data)
+//     })
+// })
 
-fetch('http://localhost:3000/weather?address=boston').then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error)
-        }
-        else{
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
+// fetch('http://localhost:3000/weather?address=boston').then((response)=>{
+//     response.json().then((data)=>{
+//         if(data.error){
+//             console.log(data.error)
+//         }
+//         else{
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// })
 
 
 const weatherForm = document.querySelector('form')
@@ -30,7 +30,7 @@ weatherForm.addEventListener('submit',(e)=>{
     const location = search.value
     messageOne.textContent = 'Loading...' 
     messageTwo.textContent = ''
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             messageOne.textContent = data.error
